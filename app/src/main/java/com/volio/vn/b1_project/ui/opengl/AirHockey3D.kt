aa0aec66545c6202b1613de5d802bd429069a440
HienDx21 <hiendx@volio.com>
2024-03-25 17:05:36 +0700

    load image

--- app/src/main/java/com/volio/vn/b1_project/ui/opengl/AirHockey3D.kt
+++ app/src/main/java/com/volio/vn/b1_project/ui/opengl/AirHockey3D.kt
@@ -3,44 +3,51 @@ package com.volio.vn.b1_project.ui.opengl
 import android.R.attr.height
 import android.R.attr.width
 import android.opengl.GLES20
+import android.opengl.Matrix.multiplyMM
 import android.opengl.Matrix.orthoM
+import android.opengl.Matrix.perspectiveM
+import android.opengl.Matrix.rotateM
+import android.opengl.Matrix.setIdentityM
+import android.opengl.Matrix.translateM
 import com.volio.vn.common.utils.getScreenHeight
 import com.volio.vn.common.utils.getScreenWidth
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 
-class AirHockey {
+class AirHockey3D {
 
     val color = floatArrayOf(1.0f, 1.0f, 1.0f, 1.0f)
     val colorLine = floatArrayOf(1.0f, 1.0f, 0f, 1.0f)
 
     val tableVerticesWithTriangles = floatArrayOf(
-            // Triangle 1
-            -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-            0.5f, 0.5f, 1.0f, 1.0f, 1.0f,
-            -0.5f, 0.5f, 1.0f, 1.0f, 1.0f,
-            // Triangle 2
-            -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-            0.5f, -0.5f, 1.0f, 1.0f, 1.0f,
-            0.5f, 0.5f, 1.0f, 1.0f, 1.0f,
-            // Line 1
-            -0.5f, 0f, 1.0f, 0f, 0f,
-            0.5f, 0f, 1.0f, 0f, 0f,
+
+            // Order of coordinates: X, Y, Z, W, R, G, B
+// Triangle Fan
+            0f, 0f, 0f, 1.5f, 1f, 1f, 1f,
+            -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+// Line 1
+            -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+            0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+
             // Mallets
-            0f, -0.25f, 1.0f, 0f, 0f,
-            0f, 0.25f, 1.0f, 0f, 0f,
+            0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f,
+            0f, 0.4f, 0f, 1.75f, 1f, 0f, 0f
     )
 
     val vertexData by lazy {
-        ByteBuffer.allocateDirect(tableVerticesWithTriangles.size * BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer().apply {
-                    put(tableVerticesWithTriangles)
-                    position(0)
-                }
+        ByteBuffer.allocateDirect(tableVerticesWithTriangles.size * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer().apply {
+            put(tableVerticesWithTriangles)
+            position(0)
+        }
     }
 
     private val projectionMatrix = FloatArray(16)
+    private val modelMatrix = FloatArray(16)
 
     private val vertexShaderCode = """
                            uniform mat4 u_Matrix;
@@ -83,13 +90,7 @@ class AirHockey {
         }
     }
 
-    fun draw() {
-        GLES20.glUseProgram(mProgram)
-
-        val postion = GLES20.glGetAttribLocation(mProgram, "a_Position")
-        val color = GLES20.glGetAttribLocation(mProgram, "a_color")
-        val uMatrixLocation = GLES20.glGetUniformLocation(mProgram, "u_Matrix")
-
+    fun setAspectRatio() {
         val aspectRatio: Float = if (getScreenWidth() > getScreenHeight()) getScreenWidth().toFloat() / getScreenHeight().toFloat() else getScreenHeight().toFloat() / getScreenWidth().toFloat()
         if (getScreenWidth() > getScreenHeight()) {
             // Landscape
@@ -98,34 +99,44 @@ class AirHockey {
             // Portrait or square
             orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
         }
+    }
+
+    fun setModelMatrix() {
+
+        perspectiveM(projectionMatrix,0, 45f, getScreenWidth().toFloat() / getScreenHeight().toFloat(), 1f, 10f)
+
+        setIdentityM(modelMatrix, 0)
+
+        translateM(modelMatrix, 0, 0f, 0f, -3.2f)
+        rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f)
+
+        val temp = FloatArray(16)
+        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0)
+        System.arraycopy(temp, 0, projectionMatrix, 0, temp.size)
+
+    }
+
+    fun draw() {
+        GLES20.glUseProgram(mProgram)
+
+        val postion = GLES20.glGetAttribLocation(mProgram, "a_Position")
+        val color = GLES20.glGetAttribLocation(mProgram, "a_color")
+        val uMatrixLocation = GLES20.glGetUniformLocation(mProgram, "u_Matrix")
+
 
         GLES20.glEnableVertexAttribArray(postion)
         vertexData.position(0)
 
-        GLES20.glVertexAttribPointer(
-                postion,
-                2,
-                GLES20.GL_FLOAT,
-                false,
-                POSITION_COMPONENT_COUNT * 4,
-                vertexData
-        )
+        GLES20.glVertexAttribPointer(postion, 2, GLES20.GL_FLOAT, false, POSITION_COMPONENT_COUNT * 4, vertexData)
 
         GLES20.glEnableVertexAttribArray(color)
-        vertexData.position(2)
+        vertexData.position(4)
 
-        GLES20.glVertexAttribPointer(
-                color,
-                3,
-                GLES20.GL_FLOAT,
-                false,
-                POSITION_COMPONENT_COUNT * 4,
-                vertexData
-        )
+        GLES20.glVertexAttribPointer(color, 3, GLES20.GL_FLOAT, false, POSITION_COMPONENT_COUNT * 4, vertexData)
 
         GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0)
 
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
         GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2)
         GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 2)
 
@@ -134,7 +145,7 @@ class AirHockey {
 
 
     companion object {
-        const val POSITION_COMPONENT_COUNT = 5
+        const val POSITION_COMPONENT_COUNT = 7
         const val BYTES_PER_FLOAT = 4
     }
 }
--- app/src/main/java/com/volio/vn/b1_project/ui/opengl/MyGLRenderer.kt
+++ app/src/main/java/com/volio/vn/b1_project/ui/opengl/MyGLRenderer.kt
@@ -7,19 +7,23 @@ import javax.microedition.khronos.opengles.GL10
 
 class MyGLRenderer : GLSurfaceView.Renderer {
 
-    private lateinit var mAirHockey: AirHockey
+   // private lateinit var mAirHockey: AirHockey
+    private lateinit var mAirHockey: AirHockey3D
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         //   GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f)
-        mAirHockey = AirHockey()
+        mAirHockey = AirHockey3D()
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
+        mAirHockey.setAspectRatio()
+        mAirHockey.setModelMatrix()
     }
 
     override fun onDrawFrame(gl: GL10?) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
         mAirHockey.draw()
+
     }
 }
